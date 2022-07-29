4790e8415d3c2ea8c24e1583d8b5ec34d266b30d
anton <asidorin1978@gmail.com>
2022-07-29 23:00:15 +0700

    create pages: search

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request
 
 from app.DAO.post_dao import PostDAO
 from app.DAO.comments_dao import CommentsDAO
@@ -13,7 +13,16 @@ comments_dao = CommentsDAO(DATA_PATH_COMMENTS)
 @post_blueprint.get('/')
 def all_posts_page():
     all_posts = posts_dao.get_all()
-    return render_template('index.html', posts=all_posts)
+    query = request.args.get("s", "")
+    if query == "":
+        posts_search = []
+    else:
+        posts_search = posts_dao.search_in_content(query)
+    return render_template('index.html',
+                           posts=all_posts,
+                           posts_search=posts_search,
+                           query=query,
+                           len_posts=len(posts_search))
 
 
 @post_blueprint.get('/post/<int:pk>')
@@ -29,3 +38,13 @@ def one_posts_page(pk):
 def page_by_user_name(user_name):
     posts = posts_dao.get_by_poster(user_name)
     return render_template('user-feed.html', posts=posts, user_name=user_name)
+
+
+@post_blueprint.get('/search/')
+def page_search():
+    query = request.args.get("s", "")
+    if query == "":
+        posts = []
+    else:
+        posts = posts_dao.search_in_content(query)
+    return render_template('search.html', posts=posts, query=query, len_posts=len(posts))
